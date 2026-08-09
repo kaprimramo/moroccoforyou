bd68e21704b712c34e9729bb87c7e32f536d1fc9
Omar <omarlarou@gmail.com>
2026-08-09 13:51:05 +0000

    Add Casablanca Airport Guide CMN pillar EN/FR/AR

--- lib/blog-content/index.ts
+++ lib/blog-content/index.ts
@@ -39,3 +39,4 @@ import './34-casablanca-to-rabat-by-car';
 import './35-best-places-to-dine-in-casablanca';
 import './36-casablanca-travel-guide';
 import './37-chefchaouen-travel-guide';
+import './38-casablanca-airport-guide-cmn';
